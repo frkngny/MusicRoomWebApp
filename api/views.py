c1154c1f1702725d4b154003c162159d517879d0
frkngny <frkang35@gmail.com>
2024-02-11 13:23:55 +0300

    Setup Rest Framework
    Create DB model
    Create View
    Create serializer to view DB objects

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 
-# Create your views here.
-def main(request):
-    return HttpResponse("Hello")
+# ListAPIView -> Returns all roms in DB as list of JSON
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
